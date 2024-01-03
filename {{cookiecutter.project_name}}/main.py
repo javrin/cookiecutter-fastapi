16defb3e42de87f3b10212cb81417965db4dff30
javrin <jawabiscuit@users.noreply.github.com>
2024-01-03 12:55:12 -0500

    Run formatter

--- {{cookiecutter.project_name}}/main.py
+++ {{cookiecutter.project_name}}/main.py
@@ -1,12 +1,13 @@
+from dotenv import load_dotenv
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
-from dotenv import load_dotenv
 
 __version__ = "{{cookiecutter.project_version}}"
 
 
 load_dotenv()
+
 
 async def homepage(request) -> JSONResponse:
     return JSONResponse({"hello": "world"})
